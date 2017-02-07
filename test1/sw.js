4c4487480d2c51df6b55972ebab6bf0d44383c39
sapics <gv.nishino@gmail.com>
2017-02-07 20:53:31 +0900

    A

--- test1/sw.js
+++ test1/sw.js
@@ -3,7 +3,10 @@ self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		fetch(
 			event.request.url.indexOf('.js') !== -1 ?
-			'/test1/sw.js' : '/test1/test.png'
+			'/test1/sw.js' :
+			event.request.url.indexOf('.png') !== -1 ?
+			'/test1/test.png' :
+			'/test1/index.html'
 		)
 	)
 });
